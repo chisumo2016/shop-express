737aedf17bfcf978c069e887d3a8b5ef4a3b24ab
chisumo <bchisumo74@yahoo.com>
2021-08-14 22:54:52 +0100

    Build the Navbar UI Component

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+    <Navbar />
+</template>
+<style scoped>
+
+</style>
+<script>
+
+
+import Navbar from "../Components/Navbar";
+
+    export default {
+        components: {
+            Navbar
+        },
+
+        props: {},
+
+        // data(){
+        //     return{
+        //         isAccountLinkHovered: false,
+        //     }
+        // }
+    }
+</script>
